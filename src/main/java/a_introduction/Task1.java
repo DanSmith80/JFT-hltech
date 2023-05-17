df84dc35589b9b866614f68c3986dc8b1480d923
Michal Dobrzycki <dobrzycki@gmail.com>
2023-05-17 08:01:13 +0200

    Push arrays

--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -2,6 +2,5 @@ package a_introduction;
 
 public class Task1 {
     public static void main(String[] args) {
-
     }
 }
---
+++ src/main/java/a_z_arrays/Task1.java
@@ -0,0 +1,7 @@
+package a_z_arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double[] temps = {30, 29, 14, 42, -4, -10, 8, 14, 32, 11, 8, 0, 0};
+    }
+}
